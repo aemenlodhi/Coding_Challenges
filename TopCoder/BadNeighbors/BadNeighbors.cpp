834e1c244a15677fb601e1391265bafdfb580341
aemenlodhi <aemen.lodhi@gmail.com>
2014-08-14 15:16:07 -0400

    correct version added

--- TopCoder/BadNeighbors/BadNeighbors.cpp
+++ TopCoder/BadNeighbors/BadNeighbors.cpp
@@ -4,36 +4,32 @@
 using namespace std;
 typedef int ui;
 
+
+int maxDon(vector<int>& don,int i,vector<int>& md){
+
+	if(i<0)
+		return 0;
+
+	if(md[i]!=0)
+		return md[i];
+	
+	return md[i]=max(maxDon(don,i-1,md),maxDon(don,i-2,md)+don[i]);
+
+}
+
 int main(){
 
 	vector<ui> input;
 	ui temp;
 
-
 	while(scanf("%d\n",&temp)!=EOF)
                input.push_back(temp);
 	
-	vector<ui> don(input.size(),0);
+	vector<ui> v1(input.begin(),input.end()-1),v2(input.begin()+1,input.end());
+	
+	vector<ui> md1(v1.size(),0), md2(v2.size(),0);
 
-	ui maxdon=0;
-
-	for(ui i=0;i<input.size();i++){
-		
-		temp=0;
-		if((i-2)>=0)
-			temp=don[i-2];	
-			
-		don[i]=input[i]+temp;
-		
-		if(input.size()%2==1 && i==input.size()-1)
-			don[i]-=don[0];
-		
-		if(don[i]>maxdon)
-			maxdon=don[i];
-
-	}
-
-	printf("Max donation: %d\n",maxdon);
+	printf("Max donation: %d\n",std::max(maxDon(v1,v1.size()-1,md1),maxDon(v2,v2.size()-1,md2)));
 	
 	return 0;
 }
